868dacec52baf601f5209f2854a83b92313c5aaf
NoobieCo <47449057+NoobieCo@users.noreply.github.com>
2020-03-06 11:57:02 +0530

    New Commit
    deleted some configurations

--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
 const express = require("express");
-
 const passport = require("passport");
-
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-
 const keys = require("./config/keys");
-
 const app = express();
 
 passport.use(
   new GoogleStrategy(
     {
       clientID: keys.googleClientID,
-
       clientSecret: keys.googleClientSecret,
-
       callbackURL: "/auth/google/callback"
     },
 
@@ -36,10 +30,6 @@ app.get(
 );
 
 app.get("/auth/google/callback", passport.authenticate("google"));
-
-//ClientId-614284390318-v0cupnr22tt756allu1v3pokis00io9c.apps.googleusercontent.com
-
-//ClientSecret-nWMk4MLlvvof62fPte2e9cUH
 
 app.get("/greeting", (req, res) => {
   res.send({ bye: "sagar" });
